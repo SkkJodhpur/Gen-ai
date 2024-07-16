f52815ecd1a9eb54e449b67d0a362314eb23b776
Er. Shailesh Kumar Khanchandani <93039945+SkkJodhpur@users.noreply.github.com>
2024-07-16 16:58:49 +0530

    E-Commerce-Chatbot
    E-Commerce-Chatbot

---
+++ E-Commerce-Chatbot/ecommbot/retrieval_generation.py
@@ -0,0 +1,48 @@
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+#from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+from ecommbot.ingest import ingestdata
+
+
+def generation(vstore):
+    retriever = vstore.as_retriever(search_kwargs={"k": 3})
+
+    PRODUCT_BOT_TEMPLATE = """
+    Your ecommercebot bot is an expert in product recommendations and customer queries.
+    It analyzes product titles and reviews to provide accurate and helpful responses.
+    Ensure your answers are relevant to the product context and refrain from straying off-topic.
+    Your responses should be concise and informative.
+
+    CONTEXT:
+    {context}
+
+    QUESTION: {question}
+
+    YOUR ANSWER:
+    
+    """
+
+
+    prompt = ChatPromptTemplate.from_template(PRODUCT_BOT_TEMPLATE)
+
+    llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    return chain
+
+if __name__=='__main__':
+    vstore = ingestdata("done")
+    chain  = generation(vstore)
+    print(chain.invoke("can you tell me the best bluetooth buds?"))
+    
+    
+    
+    
